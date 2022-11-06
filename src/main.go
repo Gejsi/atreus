764c61c825d66961c083c740e94883e26db741a0
Gejsi <vgejsi@gmail.com>
2022-11-06 23:28:36 +0100

    initial commit

---
+++ src/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	app := &cli.App {
+		Commands: []*cli.Command {
+			{
+				Name: "generate",
+				Aliases: []string{"g", "gen"},
+				Usage: "Generate some throwaway data.",
+				Action: func(ctx *cli.Context) error {
+					fmt.Println("generated data")
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
